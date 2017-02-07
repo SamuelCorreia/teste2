4adf93d3bb5f59d0f7b935105782839b3d4b29d9
a15711 <a15711@d1-08-06.dped.local>
2017-02-07 15:31:18 +0000

    versão beta 0.3

--- src/teste2/Main.java
+++ src/teste2/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Olá mundo!");
+        System.out.println("Olá mundo!");// TODO code application logic here
         System.out.println("Xau mundo!");
     }
     
